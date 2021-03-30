46040df16dd5a7a8690da1002b1c50951a998d0a
karenhdzgtz <karen_hdzgtz@comunidad.unam.mx>
2021-03-29 19:59:04 -0600

    Se modifico el archivo .gitignore y se creo la carpeta template con el archivo template.py dentro

---
+++ templates/template.py
@@ -0,0 +1,34 @@
+```
+NOMBRE
+	Template generico
+	ej. AT-percentage
+
+
+VERSION
+	1.0
+
+
+AUTOR
+	Hernandez Gutierrez Ana Karen
+
+
+DESCRIPCION
+	Escribe aqui la descripcion de tu programa
+	ej. Programa que calcula el porcentaje de ata en una secuencia genomica
+
+
+CATEGORIA
+	Escribe aqui la categoria
+	ej. Secuencia genomica
+
+
+USO
+	ej.
+	% python AT-percentage.py -i filename
+
+	ejemplo
+
+	% python AT-percentage.py -i sequence.txt
+
+
+```
