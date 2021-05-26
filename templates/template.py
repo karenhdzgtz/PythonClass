a26419138faff1ab0e16c2c215b92e572927ff8a
karenhdzgtz <karen_hdzgtz@comunidad.unam.mx>
2021-05-26 18:12:36 -0500

    Se actualizo el template, agregando funciones, manejo de errores y argumentos

--- templates/template.py
+++ templates/template.py
@@ -25,8 +25,33 @@ CATEGORIA
 USO
 	[programName][-options/arguments]
 
-ARGUMENTOS / OPCIONES
-	[nombre]  [descripción]
+
+ARGUMENTOS
+	[nombre]  [descripcion]
+	[ej. -i	  datos de entrada que pide el programa]
+
+
+DATOS DE ENTRADA
+	Input:[archivos, paths, datos, etc. que se pediran al asuario]
+
+
+DATOS DE SALIDA
+	Output:[archivos, datos, etc. que recibira el usuario si se termina
+	de ejecutar correctamente el programa]
+
+
+FUNCIONES
+	[Lista con nombre de las funciones en el codigo]
+
+	<Nombre de la funcion>(argumentos):
+		
+		[codigo asociado]
+
+		return(valor que regresa)
+
+MANEJO DE ERRORES (TRY)
+	[error]  [Como se corrige]
+	[ej. "No se puede abrir el archivo"   "Revisa la ruta y existencia del archivo"]
 
 EJEMPLOS
 	[Ejemplo 1: describe el ejemplo y después indica las instrucciones. En markdown puedes usar ` o ``` para indicar código]
